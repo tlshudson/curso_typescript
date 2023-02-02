6ecff5979c793fb0b34f813b5d16dc87ff1b182c
tlshudson <hudsonteles00@gmail.com>
2023-02-01 23:40:16 -0300

    4ª atualização

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 let x: number = 10;
 
-x = 20;
+x = 12;
 
 console.log(x);
+
+//valores armazenados por inferência e annotation
+
+//Caso de Inferência
+let y = 12;
+//y = "teste"; //Não é possivel agregar outro tipo ou outro valor em uma variável já utilizada
+//pois ela já teve um valor agregado por inferência
+
+
+//caso de annotation
+//caso de variavel que ja teve um valor armazenado de maneira explicita, mostrando seu tipo
+let y: number =12;
